9e1ea9ac2b937f12ebd074562e222da3d7b3f2b7
Your Name <you@example.com>
2023-03-23 10:28:13 +0300

    error correct

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,14 +1,14 @@
 import playGame from '../index.js';
 
+const arrayRandElement = (arr = ['+', '-', '*']) => {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
 const brainCalc = () => {
   const noteToEvenGame = 'What is the result of the expression?';
 
   const getRandomNumber = () => Math.round(Math.random() * 10);
-
-  const arrayRandElement = (arr = ['+', '-', '*']) => {
-    const rand = Math.floor(Math.random() * arr.length);
-    return arr[rand];
-  };
 
   const taskToEvenGame = () => {
     const num1 = getRandomNumber();
