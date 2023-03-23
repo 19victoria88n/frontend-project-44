ea3a8b2810ac2c596ed454452241fbb32067e91f
Your Name <you@example.com>
2023-03-23 16:30:25 +0300

    correct

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,21 +5,25 @@ const brainProgression = () => {
 
   const getRandomNumber = () => Math.round(Math.random() * 9);
 
-  const arrayRand = (arrRand = [getRandomNumber()], osnovanie = getRandomNumber()) => {
-    for (let i = 1; i < 10; i += 1) {
-      arrRand[i] = arrRand[i - 1] + osnovanie;
+  const randProgression = (num = getRandomNumber(10), osnovanie = getRandomNumber(10)) => {
+    const arrRand = [];
+    let firstElement = num;
+    for (let i = 1; i < 11; i += 1) {
+      arrRand.push(firstElement);
+      firstElement += osnovanie;
     }
     return arrRand;
   };
+
   const taskToEvenGame = () => {
     let correctAnswer = 0;
-    const newArrr = (newArr = arrayRand(), num1 = getRandomNumber()) => {
+    const progression = (newArr = randProgression(), num1 = getRandomNumber()) => {
       correctAnswer = newArr[num1];
       newArr[num1] = '..';
       return newArr.join(' ');
     };
 
-    const arr = newArrr();
+    const arr = progression();
     const question = `Question: ${arr}`;
 
     return [question, correctAnswer];
