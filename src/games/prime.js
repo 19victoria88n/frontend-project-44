e650e117050f22cb31d8c88659802e7021484f01
Your Name <you@example.com>
2023-03-23 11:59:58 +0300

    error correct

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,40 +1,30 @@
 import playGame from '../index.js';
 
 const brainPrime = () => {
-  const noteToEvenGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const noteToPrimeGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const getRandomNumber = () => Math.round(Math.random() * 10);
 
-  const isPrime = (n) => {
-    let correctAnswer = 0;
-    let sum = 0;
-    for (let i = 2; i < n; i += 1) {
-      if (n % i === 0 && i < n) {
-        sum += 1;
+  function isPrime(num) {
+    if (num === 1) {
+      return 'no';
+    }
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        return 'no';
       }
     }
-    if (sum < 1) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-    if (n === 1) {
-      correctAnswer = 'no';
-    }
-    if (n === 2) {
-      correctAnswer = 'yes';
-    }
-    return correctAnswer;
-  };
+    return 'yes';
+  }
 
-  const taskToEvenGame = () => {
+  const taskToPrimeGame = () => {
     const num = getRandomNumber();
     const question = `${num}`;
-    let correctAnswer = isPrime(num);
+    const correctAnswer = isPrime(num);
     return [question, correctAnswer];
   };
 
-  playGame(noteToEvenGame, taskToEvenGame);
+  playGame(noteToPrimeGame, taskToPrimeGame);
 };
 
 export default brainPrime;
