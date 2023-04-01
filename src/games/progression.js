4f9319598219a6ffbadcdfab64373a7f57ebb60f
Your Name <you@example.com>
2023-04-01 11:28:51 +0300

    correction

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,7 +7,7 @@ const rules = 'What number is missing in the progression?';
 const randProgression = (first, osnovanie, length) => {
   const randProgression = [];
   let firstElement = first;
-  for (let i = 1; i < length; i += 1) {
+  for (let i = 1; i <= length; i += 1) {
     randProgression.push(firstElement);
     firstElement += osnovanie;
   }
@@ -17,9 +17,9 @@ const randProgression = (first, osnovanie, length) => {
 const runProgressionGame = () => {
   const first = getRandomInRange(0, 10);
   const osnovanie = getRandomInRange(0, 10);
-  const length = 11;
+  const length = getRandomInRange(5, 10);
   const progression = randProgression(first, osnovanie, length);
-  const hiddenIndex = getRandomInRange(0, 10);
+  const hiddenIndex = getRandomInRange(0, length - 1);
   const answer = (progression[hiddenIndex]).toString();
   progression[hiddenIndex] = '..';
   const question = `Question: ${progression.join(' ')}`;
